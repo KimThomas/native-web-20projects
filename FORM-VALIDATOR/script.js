2dd1cf90cd5a220fef8902abf4db6e53c97b48eb
ThomasKim <junhe420@qq.com>
2022-06-21 17:00:57 +0800

    review form-validator project

--- FORM-VALIDATOR/script.js
+++ FORM-VALIDATOR/script.js
@@ -1,98 +1,100 @@
 const form = document.getElementById('form');
-const userName = document.getElementById('user-name');
-const email = document.getElementById('email');
-const pwd = document.getElementById('password');
-const pwdConfirm = document.getElementById('password-confirm');
-const formInputs = form.querySelectorAll('input');
-const inputIndex = {};
-const inputArr = [];
+const userNameEle = document.getElementById('user-name');
+const emailEle = document.getElementById('email');
+const pwdEle = document.getElementById('password');
+const pwdConfirmEle = document.getElementById('password-confirm');
+const minCharacter = 6;
+const maxCharacter = 15;
+const RegEmail =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-// form submit event listener
-// 1.require all
-// 2.length:user-name\pwd\pwd-confirm; email:validate
-// 3.compare:pwd\pwd-confirm
+const errorMsgObj = {
+  'required': 'is need required',
+  'characterLength': 'is more than 6 and less than 20 character',
+  'mailValid': 'error email address',
+  'pwdConfirm': 'the two password does not same'
+}
+
+function initInputCheckStatus(...inputArr) {
+  inputArr.forEach(input => {
+    input.checkNext = true;
+  });
+};
+
+function addStatusClass(...inputArr) {
+  inputArr.forEach(input => {
+    if (input.checkNext) {
+      input.parentElement.className = 'input-box success';
+    } else {
+      input.parentElement.className = 'input-box error';
+    }
+  })
+};
+
+function addErrMsg(ele, type) {
+  ele.checkNext = false;
+  const small = ele.parentElement.querySelector('small');
+  const label = ele.parentElement.querySelector("label");
+  switch (type) {
+    case 'required':
+      small.innerText = `${label.innerText} ${errorMsgObj.required}`;
+      break;
+    case 'characterLength':
+      small.innerText = `${label.innerText} ${errorMsgObj.characterLength}`;
+      break;
+    case 'mailValid':
+      small.innerText = `${errorMsgObj.mailValid}`;
+      break;
+    case 'pwdConfirm':
+      small.innerText = `${errorMsgObj.pwdConfirm}`;
+      break;
+  }
+}
+
 form.addEventListener('submit', e => {
   e.preventDefault();
-  formInputs.forEach((input, index) => {
-    inputArr[index] = {
-      ele: input,
-      checkFlag: true
-    }
-    inputIndex[input.id] = index;
-  });
-  checkRequire(inputArr);
-  checkLength(inputArr[inputIndex[userName.id]], 3, 15);
-  checkLength(inputArr[inputIndex[pwd.id]], 6, 20);
-  checkLength(inputArr[inputIndex[pwdConfirm.id]], 6, 20);
-  checkEmail(inputArr[inputIndex[email.id]]);
-  comparePwd(inputArr[inputIndex[pwd.id]], inputArr[inputIndex[pwdConfirm.id]]);
+  initInputCheckStatus(userNameEle, emailEle, pwdEle, pwdConfirmEle);
+  checkRequired(userNameEle, emailEle, pwdEle, pwdConfirmEle);
+  checkCharacterLength(userNameEle, pwdEle, pwdConfirmEle);
+  checkEmailAddress(emailEle);
+  checkPwdConfirm(pwdEle, pwdConfirmEle);
+  addStatusClass(userNameEle, emailEle, pwdEle, pwdConfirmEle);
 });
 
-//compare pwd and pwd-confirm
-function comparePwd(pwd, pwdConfirm) {
-  if (pwd.checkFlag && pwdConfirm.checkFlag) {
-    if (pwd.ele.value !== pwdConfirm.ele.value) {
-      showError(pwdConfirm.ele, `${spliceMsg(pwd.ele.id)} must same to the password`)
-    }
-  }
-}
-
-//check email address valid
-function checkEmail(input) {
-  if (input.checkFlag) {
-    const validResult = String(input.ele.value)
-        .toLowerCase()
-        .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    if (!validResult) {
-      showError(input.ele, `${spliceMsg(input.ele.id)} is not valid email address`);
-    }
-  }
-}
-
-// check input value length
-function checkLength(input, min, max) {
-  if (input.checkFlag) {
-    if (input.ele.value.length < min) {
-      showError(input.ele, `${spliceMsg(input.ele.id)} is more than ${min} character`);
-      input.checkFlag = false;
-    } else if (input.ele.value.length > max) {
-      showError(input.ele, `${spliceMsg(input.ele.id)} is less than ${max} character`);
-      input.checkFlag = false;
-    }
-  }
-}
-
-// check input value empty
-function checkRequire(inputArr) {
+function checkRequired(...inputArr) {
   inputArr.forEach(input => {
-    if (input.checkFlag) {
-      if (input.ele.value === '') {
-        showError(input.ele, `${spliceMsg(input.ele.id)} is required`);
-        input.checkFlag = false;
-      } else {
-        showSuccess(input.ele);
+    if (input.checkNext) {
+      if (input.value === '') {
+        addErrMsg(input, 'required');
       }
     }
   });
-}
+};
 
-// show success effect
-function showSuccess(input) {
-  input.parentElement.className = 'form-control success';
-}
+function checkCharacterLength(...inputArr) {
+  inputArr.forEach(input => {
+    if (input.checkNext) {
+      if (input.value.length < minCharacter || input.value.length > maxCharacter) {
+        addErrMsg(input, 'characterLength');
+      }
+    }
+  });
+};
 
-// show error effect
-function showError(input, msg) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = msg;
-}
+function checkEmailAddress(...inputArr) {
+  inputArr.forEach(input => {
+    if (input.checkNext) {
+      if (!String(input.value).toLowerCase().match(RegEmail)) {
+        addErrMsg(input, 'mailValid');
+      }
+    }
+  });
+};
 
-// splice error msg
-function spliceMsg(inputId) {
-  let labelName = inputId.split('-').join(' ');
-  return labelName.charAt(0).toUpperCase() + labelName.slice(1);
-}
+function checkPwdConfirm(pwdEle, pwdConfirmEle) {
+  if (pwdEle.checkNext && pwdConfirmEle.checkNext) {
+    if (pwdEle.value !== pwdConfirmEle.value) {
+      addErrMsg(pwdConfirmEle, 'pwdConfirm');
+    }
+  }
+};
